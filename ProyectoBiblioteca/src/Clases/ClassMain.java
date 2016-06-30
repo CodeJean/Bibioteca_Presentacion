3b9726b0ebecd8aafefb2bde87acb35c624f00a7
CodeJean <adidos_12@hotmail.com>
2016-06-30 16:47:37 -0500

    Primera Copia del proyecto
    Se inicia copia del proyecto al github

---
+++ ProyectoBiblioteca/src/Clases/ClassMain.java
@@ -0,0 +1,22 @@
+package Clases;
+
+import Principal.frmLoading;
+import javax.swing.UIManager;
+
+
+public class ClassMain {
+
+    
+    public static void main(String[] args) {
+        try{
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            frmLoading loading = new frmLoading();
+            loading.setSize(300,300);
+            loading.setLocationRelativeTo(null);
+            loading.setVisible(true);
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+    
+}
